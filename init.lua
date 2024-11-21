67d4a6cbbb71b1aaa8a127caab6158466fee4e6b
Edgar Coime <e.coimeii@hotmail.com>
2024-11-21 10:24:05 -0800

    feat: remove all plugins and add basic init

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
-require("config.lazy")
+vim.loader.enable()
+
+local utils = require("utils")
+
+local expected_version = "0.10.2"
+utils.is_compatible_version(expected_version)
+
+local config_dir = vim.fn.stdpath("config")
+---@cast config_dir string
+
+-- some global settings
+require("globals")
+-- setting options in nvim
+vim.cmd("source " .. vim.fs.joinpath(config_dir, "viml_conf/options.vim"))
