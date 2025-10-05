21cf1144a80fc818eb3eafdce59127c3d2f447b3
Edgar Coime <e.coimeii@hotmail.com>
2025-10-04 23:26:41 -0700

    feat: add flash

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -1,29 +1,37 @@
 -- scrollbar with context
 return {
-  {
-    "kevinhwang91/nvim-hlslens",
-    config = function()
-      require("hlslens").setup {
-        build_position_cb = function(plist, _, _, _)
-          require("scrollbar.handlers.search").handler.show(plist.start_pos)
-        end,
-      }
+    {
+        'kevinhwang91/nvim-hlslens',
+        config = function()
+            require('hlslens').setup({
+                build_position_cb = function(plist, _, _, _)
+                    require('scrollbar.handlers.search').handler.show(plist.start_pos)
+                end,
+            })
 
-      vim.cmd([[
-          augroup scrollbar_search_hide
-              autocmd!
-              autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
-          augroup END
-        ]])
-    end,
-  },
-  {
-    "petertriho/nvim-scrollbar",
-    -- cmd = "UndotreeToggle",
-    dependencies = {},
-    config = function()
-      require("scrollbar").setup()
-      require("scrollbar.handlers.gitsigns").setup()
-    end,
-  },
+            vim.cmd([[
+            augroup scrollbar_search_hide
+                autocmd!
+                autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
+            augroup END
+          ]])
+        end,
+    },
+    {
+        'petertriho/nvim-scrollbar',
+        -- cmd = "UndotreeToggle",
+        dependencies = {},
+        config = function()
+            require('scrollbar').setup()
+            require('scrollbar.handlers.gitsigns').setup()
+        end,
+    },
+    -- {
+    --     'gorbit99/codewindow.nvim',
+    --     config = function()
+    --         local codewindow = require('codewindow')
+    --         codewindow.setup()
+    --         codewindow.apply_default_keybinds()
+    --     end,
+    -- },
 }
