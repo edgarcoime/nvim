9d4d47b449864e75653e75a87e508eb180456352
Edgar Coime <e.coimeii@hotmail.com>
2025-10-14 20:26:17 -0700

    feat: disable markview and smartsplits for now

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,11 +1,11 @@
 -- For `plugins/markview.lua` users.
 return {
-    'OXY2DEV/markview.nvim',
-    lazy = true,
-    ft = { 'markdown' },
-    -- For blink.cmp's completion
-    -- source
-    dependencies = {
-        'saghen/blink.cmp',
-    },
+    -- 'OXY2DEV/markview.nvim',
+    -- lazy = true,
+    -- ft = { 'markdown' },
+    -- -- For blink.cmp's completion
+    -- -- source
+    -- dependencies = {
+    --     'saghen/blink.cmp',
+    -- },
 }
