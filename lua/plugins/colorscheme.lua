a1cbcf3ff431da1665899728f5847f474f63250a
Edgar Coime <e.coimeii@hotmail.com>
2025-10-17 16:19:43 -0700

    feat: updated for arch

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,24 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        integrations = {},
-        transparent_background = true,
-      })
-
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
+    -- {
+    --     'catppuccin/nvim',
+    --     name = 'catppuccin',
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         require('catppuccin').setup({
+    --             flavour = 'mocha',
+    --             integrations = {},
+    --             transparent_background = true,
+    --         })
+    --
+    --         vim.cmd.colorscheme('catppuccin')
+    --     end,
+    -- },
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end,
+    },
 }
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,38 +1,37 @@
 return {
-  "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
-  cmd = "Trouble",
-  keys = {
-    {
-      "<leader>xX",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+        {
+            '<leader>xX',
+            '<cmd>Trouble diagnostics toggle<cr>',
+            desc = 'Diagnostics (Trouble)',
+        },
+        {
+            '<leader>xx',
+            '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+            desc = 'Buffer Diagnostics (Trouble)',
+        },
+        {
+            '<leader>le',
+            '<cmd>Trouble symbols toggle focus=false<cr>',
+            desc = 'Symbols (Trouble)',
+        },
+        {
+            '<leader>ll',
+            '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+            desc = 'LSP Definitions / references / ... (Trouble)',
+        },
+        {
+            '<leader>xl',
+            '<cmd>Trouble loclist toggle<cr>',
+            desc = 'Location List (Trouble)',
+        },
+        {
+            '<leader>xq',
+            '<cmd>Trouble qflist toggle<cr>',
+            desc = 'Quickfix List (Trouble)',
+        },
     },
-    {
-      "<leader>xx",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
-    },
-    {
-      "<leader>cs",
-      "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "Symbols (Trouble)",
-    },
-    {
-      "<leader>cl",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
-    },
-    {
-      "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
-    },
-    {
-      "<leader>xQ",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
-    },
-  },
 }
-
